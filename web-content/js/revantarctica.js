ecdc730f9179412d5fb5134402387e1fe3fb3c6b
alexandernkingsbury <53443343+alexandernkingsbury@users.noreply.github.com>
2019-09-03 08:46:40 -0400

    Add files via upload

--- web-content/js/revantarctica.js
+++ web-content/js/revantarctica.js
@@ -3,36 +3,33 @@ let dbObject = {
     client:'',
     techTrack:''
 }
-
-document.getElementById('header').innerText = "YOUR TITLE GOES HERE";
-
+async function title(){
+    let response = await fetch("https://us-central1-cloudfunctions-251214.cloudfunctions.net/custom")
+    let data = await response.json()
+    let a = data.a
+    document.getElementById('customtitle').innerText = a;
+}
 //this assumes your cloud function will return a value named address with the address to an image, in a cloud storage bucket
 async function setUpImages(){
     let images = []
     images.push(document.getElementById('carousel-1'))
-    images.push(document.getElementById('carousel-2'))
-    images.push(document.getElementById('carousel-3'))
     images.forEach(async (value, index)=>{
         //index is the numbered image in the carousel if that matters to you
-        let response = await fetch("https://us-central1-cloudadmingcpdemos.cloudfunctions.net/helloworld")
-        
+        let response = await fetch("https://us-central1-cloudfunctions-251214.cloudfunctions.net/cloud-bucket?meme="+index)
     if(response.status <200 || response.status > 299){
         value.src = "images/penguins.jpg"
     } else {
         data =  await response.json()
-        value.src = data["WHATEVER YOU NAMED THE FIELD IN YOUR RETURN"]
+        value.src = data["image"]
     }
     })
 }
 setUpImages()
-
 document.getElementById('calc-label').innerText = "YOU CALC LABEL TEXT"
-
 document.getElementById('calc-input').type = 'text' || "YOUR INPUT TYPE, REPLACE TEXT"
-
-function calcSubmit(event){
+async function calcSubmit(event){
     event.preventDefault()
-    fetch("YOUR CALC CLOUD FUNCTION URL", {
+    let result = await fetch("https://us-central1-cloudfunctions-251214.cloudfunctions.net/input?doubleval="+document.getElementById('calc-input').value, {
         method: 'POST',
         body: JSON.stringify(document.getElementById('calc-input').value)
     })
@@ -41,20 +38,21 @@ function calcSubmit(event){
     } else {
         document.getElementById('calc-input').value = ''
     }
-
+        let data = await result.json()
+		let a = data.a
+    let div = document.getElementById('calc-container')
+    let display = document.createElement('p')
+    display.innerText = `Your Result is: ${a} `
+    div.appendChild(display)
 }
-
-
-
 async function buildTable (){
-    let objectResponse = await fetch("YOUR CLOUD FUNCTION URL FOR GETTING DATA")
+    let objectResponse = await fetch("https://us-central1-cloudfunctions-251214.cloudfunctions.net/dataselect")
     if(objectResponse.status <200 || objectResponse.status >299){
         let error =document.createElement('p')
         error.innerText = "Fetch Failed"
         document.getElementById('footer-table').appendChild(error)
     }else {
         let objectList = await objectResponse.json()
-       
         let headRow = document.createElement('tr')
         document.getElementById('object-table-head').appendChild(headRow)
         for(key in dbObject){
@@ -63,10 +61,9 @@ async function buildTable (){
             th.className = 'object-table-data'
             headRow.appendChild(th)
         }
-        
         objectList = objectList.map((e)=>{
             let newe = {};
-            for(key in dbObject){                
+            for(key in dbObject){
                 newe[key] = e[key]
             }
             return newe
@@ -82,7 +79,6 @@ async function buildTable (){
                 row.appendChild(data)
             }
         })
-        
     }
 }
 
@@ -121,14 +117,17 @@ function createObject(event){
             input.value = ''
         }
     }
-    
-    fetch('YOUR CLOUD FUNCTION URL FOR CREATING A NEW OBJECT',{
+
+    fetch('https://us-central1-cloudfunctions-251214.cloudfunctions.net/datastore',{
         method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify(newObj)
     })
 }
 
 
-
 buildTable()
 buildForm()
+                                                                            
